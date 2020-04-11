6970d4b71c8fcdc8b1aeaa7f2b36af58615d0b74
Xamc369 <61982770+Xamc369@users.noreply.github.com>
2020-04-10 21:19:41 -0500

    loginapp
    login en la app y con apiservice y token

---
+++ Veterinary.Common/Service/IApiService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Veterinary.Common.Models;
+
+namespace Veterinary.Common.Service
+{
+    public interface IApiService
+    {
+        Task<Response> GetOwnerByEmail(
+            string urlBase,
+            string servicePrefix,
+            string controller,
+            string tokenType,
+            string accessToken,
+            string email);
+
+        Task<Response> GetTokenAsync(
+            string urlBase,
+            string servicePrefix,
+            string controller,
+            TokenRequest request);
+
+    }
+}
---
+++ Veterinary.Prism/Veterinary.Prism/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,112 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Veterinary.Common.Models;
+using Veterinary.Common.Service;
+
+namespace Veterinary.Prism.ViewModels
+{
+	public class LoginPageViewModel : ViewModelBase
+	{
+        
+
+        //ATRIBUTOS PRIVADOS
+        private string _password;
+        private bool _isRunning;
+        private bool _isEnabled;
+        private DelegateCommand _loginCommand;
+        private readonly IApiService _apiService;
+
+        //CONSTRUCTOR
+        public LoginPageViewModel(INavigationService navigationService,
+                                  IApiService apiService) : base (navigationService)
+        {
+            Title = "Login";
+            IsEnabled = true;
+            _apiService = apiService;
+        }
+
+        //PROPIEDADES
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(Login));
+
+
+        public string Email { get; set; }
+
+        //se hace cuando las propiedades tienen un cambio en la viewmodel y se 
+        //va a ver reflejado en la interfaz de usuario
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => SetProperty(ref _isEnabled, value);
+        }
+
+        //METODOS PUBLICOS
+
+        //METODOS PRIVADOS
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await App.Current.MainPage.DisplayAlert("Error","You must enter an email","Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter a password", "Accept");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            var request = new TokenRequest
+            {
+                Password = Password,
+                Username = Email
+            };
+
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var response = await _apiService.GetTokenAsync(url, "/Account", "/CreateToken", request);
+
+            if (!response.IsSuccess)
+            {
+                IsEnabled = true;
+                IsRunning = false;
+                await App.Current.MainPage.DisplayAlert("Error", "Email or password incorrect.", "Accept");
+                Password = string.Empty;
+                return;
+            }
+
+            //var token = (TokenResponse)response.Result;
+
+            //var response2 = await _apiService.GetOwnerByEmail(
+            //    url,
+            //    "/api",
+            //    "/Owners/GetOwnerByEmail",
+            //    "bearer",
+            //    token.Token,
+            //    Email);
+
+            //var owner = (OwnerResponse)response2.Result;
+
+            //IsRunning = false;
+            //IsEnabled = true;
+            await App.Current.MainPage.DisplayAlert("Ok", "Fuck Yeahh!!", "Accept");
+        }
+    }
+}
