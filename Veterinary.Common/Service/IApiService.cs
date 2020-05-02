a813fb50f6ab7d200131697da48d74643c7ff5d1
Xamc369 <61982770+Xamc369@users.noreply.github.com>
2020-05-01 22:25:08 -0500

    modificarusuario
    funcionandno

--- Veterinary.Common/Service/IApiService.cs
+++ Veterinary.Common/Service/IApiService.cs
@@ -34,6 +34,12 @@ namespace Veterinary.Common.Service
             string controller,
              EmailRequest emailRequest);
 
-
+        Task<Response<object>> PutAsync<T>(
+            string urlBase,
+            string servicePrefix,
+            string controller,
+            T model,
+            string tokenType,
+            string accessToken);
     }
 }
